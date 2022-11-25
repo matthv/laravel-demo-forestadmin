452905ecf3aac774e72b86179a5ea1b162e43071
Matt <matthv@gmail.com>
2022-11-25 14:40:00 +0100

    chore: update default config

--- config/forest.php
+++ config/forest.php
@@ -1,12 +1,14 @@
 <?php
 
 return [
-    'models_directory' => env('MODEL_DIRECTORY', 'app/Models/'),
-    'models_namespace' => env('MODEL_NAMESPACE', 'App\Models\\'),
-    'json_file_path'   => env('JSON_FILE_PATH', '.forestadmin-schema.json'),
-    'api'              => [
-        'url'         => env('FOREST_URL', 'https://api.development.forestadmin.com'),
-        'secret'      => 'a33754d559dfec688726d147bbc4ff6c36eacdd771e572876a0b54deae9785e3',
-        'auth-secret' => 'GIC0R-MFcemxnZEBJz8DxQ',
-    ]
+    'models_directory'      => ['app/Models', 'app/Tiers'],
+    'json_file_path'        => env('JSON_FILE_PATH', '.forestadmin-schema.json'),
+    'send_apimap_automatic' => env('FOREST_SEND_APIMAP_AUTOMATIC', false),
+    'api'                   => [
+        'url'         => env('FOREST_URL', 'https://api.forestadmin.com'),
+        'secret'      => env('FOREST_ENV_SECRET'),
+        'auth-secret' => env('FOREST_AUTH_SECRET'),
+    ],
+    'included_models' => [],
+    'excluded_models' => [],
 ];
